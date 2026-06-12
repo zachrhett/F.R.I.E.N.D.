49798d36d25a7f15ff91c4cd8c5aaba9523a5211
zachrhett <nz78dj42c6@privaterelay.appleid.com>
2026-06-11 19:58:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-u
+const tabs = document.querySelectorAll(".tab");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tab.classList.toggle("active");
+
+    const panel = tab.nextElementSibling;
+    panel.classList.toggle("open");
+  });
+});
